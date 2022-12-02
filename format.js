25f52ef819f1ef675529d82a58b86a41d5fca2d1
Akshay Chintalapati <64036106+akshayvkt@users.noreply.github.com>
2022-12-01 22:34:52 -0800

    12/1 Commit 2
    Not your day, ChatGPT, not your day. Moving back to my old code

--- format.js
+++ format.js
@@ -1,31 +1,21 @@
-// function enableDarkMode(event) {
-//     var element = document.body;
-//     element.classList.toggle("dark");
-//         var text = event.textContent || innerText;
-//         {
-//             if(text == 'Light')
-//         {
-//             event.innerHTML = 'Dark';
-//         }
-//             else
-//         {
-//             event.innerHTML = 'Light';
-//         }
-//     }
-//   }
+function enableDarkMode(event) {
+    var element = document.body;
+    element.classList.toggle("dark");
+        var text = event.textContent || innerText;
+        {
+            if(text == 'Light')
+        {
+            event.innerHTML = 'Dark';
+        }
+            else
+        {
+            event.innerHTML = 'Light';
+        }
+    }
+  }
 
 
 
-// Get the button and the body elements
-const button = document.querySelector('button');
-const body = document.querySelector('body');
 
-// Set a flag to track the current mode (light or dark)
-let isDark = body.classList.contains('dark-mode');
 
-// When the button is clicked, toggle the mode and update the page
-button.addEventListener('click', () => {
-  isDark = !isDark;
-  body.classList.toggle('dark-mode', isDark);
-  button.innerText = isDark ? 'Light' : 'Dark';
-});
+
