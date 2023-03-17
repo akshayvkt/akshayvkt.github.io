42b3418e88fe4d625010f9b3acc3951b2babf157
Akshay Chintalapati <64036106+akshayvkt@users.noreply.github.com>
2023-03-16 23:49:30 -0700

    adding lig dark mode

--- format.js
+++ format.js
@@ -1,15 +1,15 @@
-function enableDarkMode(event) {
-    var element = document.body;
-    element.classList.toggle("dark");
-        var text = event.textContent || innerText;
-        {
-            if(text == 'Light')
-        {
-            event.innerHTML = 'Dark';
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleBtn = document.getElementById("toggleBtn");
+
+    toggleBtn.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+        const moonIcon = '<i class="fas fa-moon white-moon"></i>';
+        const sunIcon = '<i class="fas fa-sun yellow-sun"></i>';
+
+        if (document.body.classList.contains("dark-mode")) {
+            toggleBtn.innerHTML = sunIcon;
+        } else {
+            toggleBtn.innerHTML = moonIcon;
         }
-            else
-        {
-            event.innerHTML = 'Light';
-        }
-    }
-  }
+    });
+});
